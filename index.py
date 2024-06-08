d33a679da8f32cd370b72a37831673787ff724e8
africia <keruboafricia@gmail.com>
2024-06-08 20:48:06 +0300

    python file

---
+++ index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+#variables
+name= "Africia"
+num1= 20
+num2= 30
+
+#data types
+list = 1,2,3.0,4
+list = 'Berries','vanilla','Banans','Apples'
+
+Dictionaries = "Mouse:Rat", "Mouse:device"
+Age = 'John:20', 'Mary:18'
+
+print(name)
+print(num2)
+print(list)
+print(Age)
+
